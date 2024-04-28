efd39f1c72d2a4dda0dd3dd40ff10c432277caf9
alexzanderallen01 <alexzanderallen01@gmail.com>
2024-04-27 23:36:58 -0600

    Completed prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade percentage: ");
+        string answer = Console.ReadLine();
+        int gradePercent = int.Parse(answer);
+        string grade = "";
+
+        if (gradePercent >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            grade = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            grade = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Grade: {grade}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("You passed your class! Good job");
+        }
+        else
+        {
+            Console.WriteLine("You failed, but with everything you learned, you can pass next time");
+        }
     }
 }
